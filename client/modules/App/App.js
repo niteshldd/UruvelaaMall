f076911a9aff853bd51163452cdf309527b7ef89
Nitesh.ldd <nitesh.ldd@gmail.com>
2018-08-02 10:41:38 +0530

    Changes heading

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -40,8 +40,8 @@ export class App extends Component {
         {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
         <div>
           <Helmet
-            title="MERN Starter - Blog App"
-            titleTemplate="%s - Blog App"
+            title="Uruvelaa Mall"
+            titleTemplate="%s - Uruvelaa Corporation"
             meta={[
               { charset: 'utf-8' },
               {
